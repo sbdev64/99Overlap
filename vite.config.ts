70a6402898751a156044e6a1a38fd0078de8f134
sbdev64 <sergibarris.94@gmail.com>
2026-09-12 21:40:58 +0200

    feat: add Biome + Lefthook + commitlint (#4)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
